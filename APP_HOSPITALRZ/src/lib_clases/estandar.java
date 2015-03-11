792b8a368a13cf1167f8c726a3f5570cd0cd0a40
LUIS <LUIS@LUIS-PC>
2015-03-11 09:25:32 -0500

    Revert "estructura de clases y formulario principal"
    This reverts commit 4c5e138bd28bc630233b0da64cc6d683091fe41a.

--- APP_HOSPITALRZ/src/lib_clases/estandar.java
+++ APP_HOSPITALRZ/src/lib_clases/estandar.java
@@ -10,5 +10,23 @@ package lib_clases;
  * @author LUIS
  */
 public class estandar {
+    int id;
+    int idestado;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdestado() {
+        return idestado;
+    }
+
+    public void setIdestado(int idestado) {
+        this.idestado = idestado;
+    }
     
 }
